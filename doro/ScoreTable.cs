8c3a22c7cbc069759a64dddea02741fd96d8a854
TheIdleGrinder <119699620+TheIdleGrinder@users.noreply.github.com>
2024-03-31 17:46:34 +0200

    Added datastructure project and referenced it in the app

---
+++ doro/ScoreTable.cs
@@ -0,0 +1,12 @@
+﻿namespace doro
+{
+    public class ScoreTable
+    {
+        public Tasks Tasks { get; set; } = new();
+    }
+
+    public class Tasks
+    {
+        public int Forest { get; set; } = 0;
+    }
+}
